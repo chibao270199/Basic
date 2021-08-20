b7d616aa3230459f7efad6dfe593b6571857038a
chibao270199 <nguyenchibao27011999@gmail.com>
2021-08-20 15:56:42 +0700

    20/08/2021

---
+++ Python/Bai_3/tinh_ket_qua_2.py
@@ -0,0 +1,19 @@
+print (("Nguyễn Chí Bảo").center(78,'-'))
+x = 10
+y = 4
+print ( 'x = %d, y = %d'%(x,y))
+equivelence = x==y
+print ('x==y is',equivelence)
+equivelence = x!=y
+print ('x!=y is',equivelence)
+equivelence = x > y
+print ('x>y is',equivelence)
+x = 8
+y = 9
+print ( 'x = %d, y = %d'%(x,y))
+equivelence = x>=y
+print ('x>=y is',equivelence)
+equivelence = x<y
+print ('x<y is',equivelence)
+equivelence = x <= y
+print ('x<=y is',equivelence)
